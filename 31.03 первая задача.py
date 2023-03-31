7037fa46eb412283cd2d9bccff7a239811927de9
rolexov <125002034+rolexov@users.noreply.github.com>
2023-03-31 11:07:28 +0300

    Add files via upload

---
+++ 31.03 первая задача.py
@@ -0,0 +1,8 @@
+a=int(input("Первое число"))
+b=int(input("Второе число"))
+c=int(input("Сумма"))
+if a+b==c:
+    print("Сумма чисел верна")
+elif a+b!=c:
+    print("Сумма неверна")
+    print(a+b)
