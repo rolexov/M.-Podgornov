cf15acc1cb1ca24e1c9ee68d8b3903216b6b4612
rolexov <125002034+rolexov@users.noreply.github.com>
2023-04-11 11:21:42 +0300

    Add files via upload

---
+++ main (6).py
@@ -0,0 +1,55 @@
+a=10
+i=10
+
+while i<23:
+    i=i+1
+    print(a*25.4)
+    a=a+1
+    
+    a=1
+i=1
+while i<21:
+    i=i+1
+    print(a*70)
+    a=a+1
+    
+e=2.7
+i=0
+
+while i<21:
+    i=i+1
+    print(e**i)
+i=0.1
+
+while i<21.1:
+    i=i+1
+    print(i)
+i=2.0
+
+while i<2.9:
+    i=i+0.1
+    print(i)
+i=0 
+while i<20:
+    i=i+2 
+    print(i)
+    
+    i=-1
+while i<15:
+    i=i+2 
+    print(i)
+i=2 
+while i<21:
+    print(i**0.5)
+    i=i+2
+i=2
+c=int(input("Цена"))
+while i<11:
+    print(c*i)
+    i=i+1
+    
+    a=int(input("Начало"))
+b=int(input("Конец"))
+while a<b:
+    print(a**0.5)
+    a=a+1
